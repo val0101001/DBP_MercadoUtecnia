0e83f3bef692274cea0724b0359d11302e9157a1
Valeria Celeste Valdez Campoverde <105948771+val0101001@users.noreply.github.com>
2023-05-14 20:07:04 -0500

    Add files via upload

--- Productos.php
+++ Productos.php
@@ -1,27 +1,92 @@
-</head>
-<body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.1/css/bootstrap.min.css">
+    <style>
+	body {
+	    background-image: url('https://images.wallpapersden.com/image/download/abstract-shapes-2021-minimalist_bG1lZm6UmZqaraWkpJRnamtlrWZpaWU.jpg');
+	    background-repeat: no-repeat;
+	    background-size: cover;
+	}
+        .form-container {
+            background-color: #fff;
+            border-radius: 10px;
+            box-shadow: 0px 0px 20px 0px rgba(0,0,0,0.2);
+            padding: 30px;
+        }
+        .form-container h1 {
+            font-size: 2.5rem;
+            font-weight: bold;
+            color: #007bff;
+        }
+        .form-container label {
+            font-size: 1.2rem;
+            font-weight: bold;
+            color: #555;
+        }
+        .form-container input[type="nombre"], .form-container input[type="password"] {
+            border-radius: 5px;
+            padding: 10px;
+            border: none;
+            box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.2);
+        }
+        .form-container input[type="nombre"]:focus, .form-container input[type="password"]:focus {
+            box-shadow: 0px 0px 10px 0px rgba(0,0,255,0.5);
+            outline: none;
+        }
+        .form-container .btn-primary {
+            background-color: #007bff;
+            border: none;
+            box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.2);
+        }
+        .form-container .btn-primary:hover {
+            background-color: #0056b3;
+        }
+        .form-container .mt-2 {
+            font-size: 0.9rem;
+            color: #888;
+        }
+        .form-container .mt-2 a {
+            color: #007bff;
+            text-decoration: none;
+        }
+        .form-container .mt-2 a:hover {
+            text-decoration: underline;
+        }
+    </style>
 
-<div class='container'>
-	<div class='row'>
-		<div class='col-md-6 mx-auto'>
+<body>
+<div class="container-fluid vh-100 justify-content-center align-items-center">
+	<div class="form-container">
+		<div class="col-md-8">
 			<form id='myForm' action='' method='post'>
-				<div class='mb-3'>
+				<div class="mb-3">
 					<label class='form-label'>Numero de tarjeta</label>
+					<br>
 					<input type='number' name='array[]' class='form-control' required>
 				</div>
-				<div class='mb-3'>
+				<div class="mb-3">
 					<label class='form-label'>Fecha de expiracion</label>
+					<br>
 					<input type='month' name='array[]' class='form-control' required>
 				</div>
-				<div class='mb-3'>
+				<div class="mb-3">
 					<label class='form-label'>CCV</label>
+					<br>
 					<input type='number' min='100' max='999' name='array[]' class='form-control' required>
 				</div>
-				<button type='button' onclick='generate()' class='btn btn-primary'>Confirmar</button>
+				<div class="d-grid">
+					<button type="submit" onclick='generate()' class='btn btn-primary'>Confirmar</button>
+				</div>
 			</form>
 		</div>
 	</div>
 </div>
+</body>
 
 <script>
 function generate(){
@@ -80,9 +145,4 @@ function submit_form(form_id,method,table,unique){
 	Form.submit();
 }
 </script>
-
-</body>
 </html>
-";
-
-?>
--- check-item-exists.php
+++ check-item-exists.php
@@ -1,9 +1,72 @@
 
-<div class="container">
-  <div class="row justify-content-center">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.1/css/bootstrap.min.css">
+    <style>
+	body {
+	    background-image: url('https://images.wallpapersden.com/image/download/abstract-shapes-2021-minimalist_bG1lZm6UmZqaraWkpJRnamtlrWZpaWU.jpg');
+	    background-repeat: no-repeat;
+	    background-size: cover;
+	}
+        .form-container {
+            background-color: #fff;
+            border-radius: 10px;
+            box-shadow: 0px 0px 20px 0px rgba(0,0,0,0.2);
+            padding: 30px;
+        }
+        .form-container h1 {
+            font-size: 2.5rem;
+            font-weight: bold;
+            color: #007bff;
+        }
+        .form-container label {
+            font-size: 1.2rem;
+            font-weight: bold;
+            color: #555;
+        }
+        .form-container input[type="nombre"], .form-container input[type="password"] {
+            border-radius: 5px;
+            padding: 10px;
+            border: none;
+            box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.2);
+        }
+        .form-container input[type="nombre"]:focus, .form-container input[type="password"]:focus {
+            box-shadow: 0px 0px 10px 0px rgba(0,0,255,0.5);
+            outline: none;
+        }
+        .form-container .btn-primary {
+            background-color: #007bff;
+            border: none;
+            box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.2);
+        }
+        .form-container .btn-primary:hover {
+            background-color: #0056b3;
+        }
+        .form-container .mt-2 {
+            font-size: 0.9rem;
+            color: #888;
+        }
+        .form-container .mt-2 a {
+            color: #007bff;
+            text-decoration: none;
+        }
+        .form-container .mt-2 a:hover {
+            text-decoration: underline;
+        }
+    </style>
+<body>
+<div class="container-fluid justify-content-center align-items-center">
+  <div class="form-container">
     <div class="col-md-8">
       <form id="myForm" method="POST">
         <div class="form-group">
+          <br><br><br>
+          <h1 class="text-center mb-4">Enter your required data</h1>
           <label for="name">Name:</label>
           <input type="text" class="form-control" id="name" name="name" required>
         </div>
@@ -15,12 +78,15 @@
           <label for="message">Message:</label>
           <textarea class="form-control" id="message" name="message" rows="5" required></textarea>
         </div>
+        <br>
+        <div class="row justify-content-center">
         <button type="submit" class="btn btn-primary">Submit</button>
+        </div>
       </form>
     </div>
   </div>
 </div>
-
+      </body>
 <script>
   const form = document.getElementById("myForm");
 
